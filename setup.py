4230c23465680791fb2db7805a54d68da705dae0
astraw <astraw@9608957c-362e-0410-90bb-ddb82fccb57f>
2007-10-04 15:42:52 +0000

    add URL to setup.py
    git-svn-id: https://code.astraw.com/motmot/trunk/motmot_utils@126 9608957c-362e-0410-90bb-ddb82fccb57f

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 
 setup(name='motmot_utils',
       description='miscellaneous utilities for the motmot camera packages',
-      version='20070318.00',
+      version='20071004.00',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
+      url='http://code.astraw.com/projects/motmot',
       license='BSD',
       packages=['motmot_utils'],
       )
