f2a9fd5aa2443e53c01b6d4ceb8dda09e1e2c2ae
Andrew Straw <strawman@astraw.com>
2009-06-12 19:05:18 -0700

    bump version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name='motmot.utils',
       description='miscellaneous utilities for the motmot camera packages',
-      version='20071230.00',
+      version='20090612',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       url='http://code.astraw.com/projects/motmot',
